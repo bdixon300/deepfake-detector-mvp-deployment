f555329778ca6fc943ead6e666cd3078b9888760
bd716 <bd716@ic.ac.uk>
2020-06-05 11:57:02 +0100

    Final code cleanup for archive

--- app.py
+++ app.py
@@ -24,26 +24,19 @@ class VGGCNN(nn.Module):
         self.drop_p = drop_p
 
         vgg = models.vgg16(pretrained=True)
-        modules = list(vgg.children())[:-1]      # delete the last fc layer.
+         # delete the last fc layer.
+        modules = list(vgg.children())[:-1]
         self.vgg = nn.Sequential(*modules)
-
-
-        # These layers might be removed?
-        """
-        self.fc1 = nn.Linear(vgg.fc.in_features, fc_hidden1)
-        self.bn1 = nn.BatchNorm1d(fc_hidden1, momentum=0.01)
-        self.fc2 = nn.Linear(fc_hidden1, fc_hidden2)
-        self.bn2 = nn.BatchNorm1d(fc_hidden2, momentum=0.01)
-        self.fc3 = nn.Linear(fc_hidden2, CNN_embed_dim)"""
         
     def forward(self, x_3d):
         cnn_embed_seq = []
         for t in range(x_3d.size(1)):
             # VGG CNN
             with torch.no_grad():
-                x = self.vgg(x_3d[:, t, :, :, :])  # VGG
-                x = x.view(x.size(0), -1)             # flatten output of conv
-
+                # VGG feature extraction
+                x = self.vgg(x_3d[:, t, :, :, :])
+                # flatten output of conv
+                x = x.view(x.size(0), -1)
             cnn_embed_seq.append(x)
 
         # swap time and sample dim such that (sample dim, time dim, CNN latent dim)
@@ -58,8 +51,10 @@ class LSTM(nn.Module):
         super(LSTM, self).__init__()
 
         self.RNN_input_size = CNN_embed_dim
-        self.h_RNN_layers = h_RNN_layers   # RNN hidden layers
-        self.h_RNN = h_RNN                 # RNN hidden nodes
+        # RNN hidden layers
+        self.h_RNN_layers = h_RNN_layers
+        # RNN hidden nodes
+        self.h_RNN = h_RNN
         self.h_FC_dim = h_FC_dim
         self.drop_p = drop_p
         self.num_classes = num_classes
@@ -68,32 +63,39 @@ class LSTM(nn.Module):
             input_size=self.RNN_input_size,
             hidden_size=self.h_RNN,        
             num_layers=h_RNN_layers,       
-            batch_first=True,       # input & output will has batch size as 1s dimension. e.g. (batch, time_step, input_size)
+            batch_first=True, # input & output will has batch size as 1s dimension. e.g. (batch, time_step, input_size)
         )
 
-        self.fc1 = nn.Linear(self.h_RNN, self.h_FC_dim)
-        self.fc2 = nn.Linear(self.h_FC_dim, self.num_classes)
+        self.fc1 = nn.Linear(self.h_RNN, 128)
+        self.bn1 = nn.BatchNorm1d(self.h_FC_dim, momentum=0.01)
+        self.fc2 = nn.Linear(self.h_FC_dim, 64)
+        self.bn2 = nn.BatchNorm1d(64, momentum=0.01)
+        self.fc3 = nn.Linear(64, self.num_classes)
 
     def forward(self, x_RNN):
         
         self.LSTM.flatten_parameters()
         RNN_out, (h_n, h_c) = self.LSTM(x_RNN, None)  
-
-        # FC layers
-        x = self.fc1(RNN_out[:, -1, :])   # choose RNN_out at the last time step
+        
+        x = self.bn1(self.fc1(RNN_out[:, -1, :])) # Use value at last time step in sequence
         x = F.relu(x)
         x = F.dropout(x, p=self.drop_p, training=self.training)
-        x = self.fc2(x)
+        x = self.bn2(self.fc2(x))
+        x = F.relu(x)
+        x = F.dropout(x, p=self.drop_p, training=self.training)
+        x = self.fc3(x)
         x = torch.sigmoid(x)
+
+
         return x
 
-# Setup detection model
+# load detection model parameters
 cnn = VGGCNN()
 cnn.cuda()
 cnn.load_state_dict(torch.load('./model/full_data_cnnmodel_for_lstm_2.pth'), strict=False)
 cnn.eval()
 lstm = LSTM()
-lstm.load_state_dict(torch.load('./model/full_data_cnnmodel_lstm_epoch_7.pth'))
+lstm.load_state_dict(torch.load('./model/extended_lstm_architecture_cnn_lstm_epoch_3.pth'))
 lstm.cuda()
 lstm.eval()
 
@@ -104,14 +106,6 @@ predictor = dlib.shape_predictor(p)
 
 
 app = flask.Flask(__name__, template_folder='templates')
-
-def stream_template(template_name, **context):
-    app.update_template_context(context)
-    t = app.jinja_env.get_template(template_name)
-    rv = t.stream(context)
-    rv.enable_buffering(5)
-    return rv
-
 
 def analyse_video(filename):
     # Setup reading video
@@ -128,6 +122,9 @@ def analyse_video(filename):
         gray = cv2.cvtColor(image, cv2.COLOR_RGB2GRAY)
         faces = detector(gray, 0)
         largest_face_size = 0
+
+        if len(faces) == 0:
+            continue
 
         for (i, face) in enumerate(faces):
             # Make the prediction and transfom it to numpy array
@@ -162,27 +159,29 @@ def analyse_video(filename):
             X = []
             print("current voter talley: {}, current frame sequences processed: {}".format(voter_tally, frame_count / 20))
             current_probability = (voter_tally.item() / (frame_count / 20)) * 100
-            yield flask.render_template('progress.html', processing=str(current_probability))
+            yield str(round(current_probability, 2))
         frame_count += 1
     print(frame_count)
     print("voter talley: {}".format(voter_tally))
     if voter_tally < (frame_count / 20) / 2:
-        #fake_vids += 1
         print("fake: video: {}".format(filename))
-        yield flask.render_template('main.html', result='Fake')
+        yield "Fake"
     else:
         #real_vids += 1
         print("real: video: {}".format(filename))
-        yield flask.render_template('main.html', result='Real')
+        yield "Real"
 
-@app.route('/', methods=['GET', 'POST'])
+@app.route('/', methods=['GET'])
 def main():
     if flask.request.method == 'GET':
         return(flask.render_template('main.html'))
-    if flask.request.method == 'POST':
-        url = flask.request.form['url']
-        filename = pafy.new(url).getbest().url
-        return flask.Response(flask.stream_with_context(analyse_video(filename)))
+
+@app.route('/detect', methods=['POST'])
+def detect():
+    url = flask.request.json['url']
+    filename = pafy.new(url).getbest().url
+    return flask.Response(flask.stream_with_context(analyse_video(filename)), mimetype='text/event-stream')
+
 
 if __name__ == '__main__':
     app.run()
